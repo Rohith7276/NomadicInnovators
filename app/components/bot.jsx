47f2c923092a6d07383655ab7e03599ad0877e76
Rohith7276 <140833328+Rohith7276@users.noreply.github.com>
2024-11-25 22:10:01 +0530

    fsd

--- app/components/bot.jsx
+++ app/components/bot.jsx
@@ -1,41 +1,11 @@
-"use client"
-import { Webchat, WebchatProvider, Fab, getClient } from "@botpress/webchat";
-import { buildTheme } from "@botpress/webchat-generator";
-import { useState } from "react";
+import React from 'react'
 
-const { theme, style } = buildTheme({
-  themeName: "prism",
-  themeColor: "#634433",
-});
-
-//Add your Client ID here ⬇️
-const clientId = "07e214ad-1aba-4a64-90a0-944c4b4677f0";
-
-
-export default function App() {
-  const client = getClient({ clientId });
-  const [isWebchatOpen, setIsWebchatOpen] = useState(false);
-
-  const toggleWebchat = () => {
-    setIsWebchatOpen((prevState) => !prevState);
-  };
-
+const bot = () => {
   return (
     <div>
-      <style>{style}</style>
-      <WebchatProvider
-        theme={theme}
-        client={client}
-      >
-        <Fab onClick={toggleWebchat} />
-        <div
-          style={{
-            display: isWebchatOpen ? "block" : "none",
-          }}
-        >
-          <Webchat />
-        </div>
-      </WebchatProvider>
+      
     </div>
-  );
+  )
 }
+
+export default bot
